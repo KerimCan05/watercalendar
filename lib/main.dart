ab5adda86bc0ed790977492a39eb1a862ef409da
Can Özkurt <kerim.can.ozkurt0@gmail.com>
2026-05-24 19:22:12 +0300

    0.1.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:my_toastify/my_toastify.dart';
 import 'package:water_calendar/calendar.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -14,7 +15,6 @@ void main() async {
   await Hive.initFlutter();
   calendarBox = await Hive.openBox('calendarBox');
   HiveCheck();
-  print(today);
   runApp(MaterialApp(
 
     theme: ThemeData(),
@@ -29,52 +29,83 @@ void main() async {
   );
 }
 
-
-class WaterCounter extends StatelessWidget {
+class WaterCounter extends StatefulWidget {
   const WaterCounter({super.key});
+  @override
+  _WaterCounterState createState() => _WaterCounterState();
+}
+
+class _WaterCounterState extends State<WaterCounter> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState(){
+    super.initState();
+    _controller.text = '';
+  } 
+
+  @override
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Center(child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
           children: [
-            SizedBox(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(hintText: 'How many milliliters?'),
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.number,
-                autofocus: true,
-                onChanged: (value) {
-                  if (value.isEmpty){
+            Center(child: Column( 
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 300,
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(hintText: 'How many milliliters?'),
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.number,
+                    autofocus: true,
+                    onChanged: (value) {
+                      if (value.isEmpty){
+                          drink = 0;
+                      }else{
+                        drink = int.parse(value);}
+                    }, 
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  ),
+                ),
+                SizedBox(height: 16,),
+                ElevatedButton(
+                  onPressed: () {if (drink != 0) {
+                      glassCount = glassCount + 1;
+                      milliliters = milliliters + drink;
+                      calendarBox.put(today, {'glass': glassCount, 'ml': milliliters});
+                      Toastify.show(
+                        context,
+                        position: ToastPosition.top,
+                        message: "You drank $drink ml!");
+                      _controller.clear();
                       drink = 0;
-                  }else{
-                    drink = int.parse(value);}
-                }, 
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              ),
-            ),
-            SizedBox(height: 16,),
-            ElevatedButton(
-              onPressed: () {
-                glassCount = glassCount + 1;
-                milliliters = milliliters + drink;
-                calendarBox.put(today, {'glass': glassCount, 'ml': milliliters});
-                print(calendarBox.get(today));},
-              child: Text('Drink!')),
-            SizedBox( height: 16,),
-            ElevatedButton(onPressed: () {Navigator.pushNamed(context, '/calendar');}, child: Text('Calendar')),
-            SizedBox(height: 16,),
-            ElevatedButton(onPressed: () {calendarBox.clear(); glassCount = 0; milliliters = 0;}, child: Text("Reset"))
+                    }},
+                  child: Text('Drink!')),
+                SizedBox( height: 16,),
+                ElevatedButton(onPressed: () {Navigator.pushNamed(context, '/calendar');}, child: Text('Calendar')),                
+              ],
+            ),),
+            Padding(
+              padding: EdgeInsetsGeometry.directional(bottom: 16),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: ElevatedButton(onPressed: () {calendarBox.clear(); glassCount = 0; milliliters = 0;}, child: Text("Reset"))
+            ),)
           ],
         ),
-      )  
     );
   }
 }
-
 
 class HiveCheck {
 
